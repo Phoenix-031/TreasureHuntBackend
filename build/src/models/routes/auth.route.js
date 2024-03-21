16d6e475162837787747ad57a074ca231cd3ad5f
Debayan Pradhan <pradhandebayan@gmail.com>
2024-03-21 10:03:12 +0530

    added amplify concepts

--- build/src/models/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-misused-promises */
-const express_1 = require("express");
-const authRouter = (0, express_1.Router)();
-exports.authRouter = authRouter;
-authRouter.post('verifyId');
